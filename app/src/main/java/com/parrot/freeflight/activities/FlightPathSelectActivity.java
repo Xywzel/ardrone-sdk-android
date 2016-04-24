815c1d422373c8d38ed8e3690e8629ffd2ddced9
Teemu Huovinen <teemu.huovinen@hotmail.fi>
2016-04-24 18:28:11 +0300

    added filename reading

--- app/src/main/java/com/parrot/freeflight/activities/FlightPathSelectActivity.java
+++ app/src/main/java/com/parrot/freeflight/activities/FlightPathSelectActivity.java
@@ -2,22 +2,70 @@ package com.parrot.freeflight.activities;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parrot.freeflight.R;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class FlightPathSelectActivity extends Activity {
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_path_select);
+
+        arrayList = new ArrayList<String>();
+        list = (ListView) findViewById(R.id.listViewFiles);
+
+        // Read svg filenames in directory
+        String path = Environment.getExternalStorageDirectory().toString()+"/FreeFlight";
+        try
+        {
+            File f = new File(path);
+            File file[] = f.listFiles();
+            for (int i=0; i < file.length; i++)
+            {
+                if (file[i].getName().endsWith(".svg"))
+                {
+                    arrayList.add(file[i].getName());
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Log.d("Files", "Path not found");
+        }
+
+        // Update UI element
+        adapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(adapter);
+
+        // Set onclick
+        list.setClickable(true);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                String fileName = list.getItemAtPosition(position).toString();
+                Log.d("Files", "clicked: " + fileName);
+            }
+        });
+
     }
 
     public void flightPathSelectOnClick(View v){
         super.onBackPressed();
-        Log.d("asd","hurdur");
     }
 
 }
